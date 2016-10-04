600244216a1f582e455360be393546bbca24182c
pglotfel <pglotfel@gmail.com>
2016-10-04 16:05:20 -0400

    Don't wrap create* functions in anon. functions

--- utilities/transformations/create_uni_to_si_mapping.m
+++ utilities/transformations/create_uni_to_si_mapping.m
@@ -30,7 +30,14 @@ function [uni_to_si_dyn, si_to_uni_states] = create_uni_to_si_mapping(varargin)
     % First mapping from SI -> unicycle.  Keeps the projected SI system at
     % a fixed distance from the unicycle model
     function dxi = uni_to_si(dxu, states)
-        N = size(dxu, 2); 
+        [M, N] = size(dxu);
+        [M_states, N_states] = size(states);
+        
+        % Dimensionality checks
+        assert(M==2, 'Column size of unicycle velocity vector (%i) must be 2', M);
+        assert(M_states==3, 'Column size of poses (%i) must be 3', M_states);
+        assert(N==N_states', 'Row size of unicycle velocities (%i) must be the same as poses (%i)', N, N_states);
+        
         dxi = zeros(2, N);        
         for i = 1:N
             dxi(:, i) = [cos(states(3, i)) -sin(states(3, i)); sin(states(3, i)) cos(states(3,i))] * T * dxu(:, i);
@@ -39,8 +46,18 @@ function [uni_to_si_dyn, si_to_uni_states] = create_uni_to_si_mapping(varargin)
 
     % Projects the single-integrator system a distance in front of the
     % unicycle system
-    function xi = si_to_uni_states_(uni_states, si_states)              
-       xi = si_states(1:2, :) - projection_distance*[cos(uni_states(3, :)) ; sin(uni_states(3, :))];
+    function xi = si_to_uni_states_(uni_states, si_states)    
+        
+        % Dimensionality checks 
+        [M, N] = size(uni_states);
+        [M_si, N_si] = size(si_states);
+        
+        assert(M==3, 'Column size of poses (%i) must be 3', M);
+        assert(M_si==2, 'Column size of SI poses (%i) must be 2', M_si);
+        assert(N==N_si, 'Row size of poses (%i) must be the same as SI poses (%i)', N, N_si);
+        
+        
+        xi = si_states(1:2, :) - projection_distance*[cos(uni_states(3, :)) ; sin(uni_states(3, :))];
     end
 end
 
