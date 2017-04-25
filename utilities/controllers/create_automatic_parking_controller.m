818456388fbbe4c642a3fa621e59dbec7e625da9
pglotfel@gmail.com <gritslab@gatech.edu>
2017-04-25 18:02:33 -0400

    fixed introduced bug

--- utilities/controllers/create_automatic_parking_controller.m
+++ utilities/controllers/create_automatic_parking_controller.m
@@ -20,8 +20,11 @@
 function [ automatic_parking_controller ] = create_automatic_parking_controller(varargin)
 
     p = inputParser;
-    addOptional(p, 'LinearVelocityGain', 1);
-    addOptional(p, 'AngularVelocityGain', 1);
+    addOptional(p, 'ApproachAngleGain', 1);
+    addOptional(p, 'DesiredAngleGain', 2.7); 
+    addOptional(p, 'RotationErrorGain', 1);
+    addOptional(p, 'PositionError', 0.01); 
+    addOptional(p, 'RotationError', 0.25);
     parse(p, varargin{:});
     
     gamma = p.Results.ApproachAngleGain; 
